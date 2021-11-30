8252f6784cfa9a9fca258960c30f7f93cbd1a926
robinvsoelen <91128213+robinvsoelen@users.noreply.github.com>
2021-11-30 10:56:11 +0100

    Deleted useless component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BlockViewComponent } from './components/block-view/block-view.component';
 import { BlockchainSceneComponent } from './components/blockchain-scene/blockchain-scene.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlockViewComponent,
     BlockchainSceneComponent,
   ],
   imports: [
